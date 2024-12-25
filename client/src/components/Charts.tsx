cb1ab2a55691869709708abb95027486d4154d8f
Jan Kulesza <kulesz.jasiek@gmail.com>
2024-12-25 23:08:37 +0100

    Fix: ModeToggle

--- client/src/components/Charts.tsx
+++ client/src/components/Charts.tsx
@@ -4,9 +4,9 @@ import { PieChartItem } from "../entities/PieChartItem";
 const Charts = () => {
   const items: PieChartItem[] = [
     { title: "Properties for sale", value: [376, 61], color: "#485cec" },
-    { title: "Properties for rent", value: [539, 27], color: "#485cec" },
-    { title: "Total customers", value: [6583, 1982], color: "#485cec" },
-    { title: "Total City", value: [90, 19], color: "#485cec" },
+    { title: "Properties for rent", value: [539, 27], color: "#ff843c" },
+    { title: "Total customers", value: [6583, 1982], color: "#30d484" },
+    { title: "Total City", value: [90, 19], color: "#ff6c8c" },
   ];
 
   return (
--- client/src/components/ModeToggle.tsx
+++ client/src/components/ModeToggle.tsx
@@ -10,10 +10,10 @@ export function ModeToggle() {
       variant="ghost"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       size="icon"
-      className="h-[48px] w-[48px]"
+      className="relative h-[48px] w-[48px]"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
