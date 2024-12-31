698d8b7b2d86649f9e019e26074374821240a177
Jan Kulesza <kulesz.jasiek@gmail.com>
2024-12-31 15:35:04 +0100

    Add route error handling

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
+import Error from "./pages/Error";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="" element={<Dashboard />} />
+          <Route index element={<Dashboard />} />
+          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/Error.tsx
@@ -0,0 +1,7 @@
+import { useRouteError } from "react-router";
+
+const Error = () => {
+  return <div>Error</div>;
+};
+
+export default Error;
