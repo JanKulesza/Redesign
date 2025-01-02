b085c524fc51f2b14c76e9739c2d1b72de5aeae8
Jan Kulesza <kulesz.jasiek@gmail.com>
2025-01-02 11:54:24 +0100

    Initialize property controller

---
+++ server/controllers/property.controller.js
@@ -0,0 +1,13 @@
+const getAllProperties = async (req, res) => {};
+const getPropertyDetail = async (req, res) => {};
+const createProperty = async (req, res) => {};
+const updateProperty = async (req, res) => {};
+const deleteProperty = async (req, res) => {};
+
+export {
+  getAllProperties,
+  getPropertyDetail,
+  createProperty,
+  updateProperty,
+  deleteProperty,
+};
