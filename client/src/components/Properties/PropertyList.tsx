75f9f5bc980bba8e38a77971ed296b1f2c50ca49
Jan Kulesza <kulesz.jasiek@gmail.com>
2025-01-10 19:45:54 +0100

    PropertyList slight style changes

--- client/src/components/Properties/PropertyList.tsx
+++ client/src/components/Properties/PropertyList.tsx
@@ -6,7 +6,7 @@ const PropertyList = () => {
   console.log(properties);
 
   return (
-    <div className="bg-background rounded-xl lg:m-10 p-2 xl:p-8 grid gap-8 xl:gap-3 lg:grid-cols-2">
+    <div className="bg-background rounded-xl md:m-2 xl:m-10 p-2 xl:p-8 grid gap-8 xl:gap-3 lg:grid-cols-2">
       {properties.map((property) => (
         <PropertyCard property={property} key={property._id} />
       ))}
--- client/src/pages/Properties.tsx
+++ client/src/pages/Properties.tsx
@@ -4,8 +4,8 @@ import PropertyList from "@/components/Properties/PropertyList";
 const Properties = () => {
   return (
     <div>
-      <div className="flex justify-between items-center lg:m-10 mb-8 p-4 md:p-0">
-        <h1 className="text-foreground text-xl md:text-4xl font-semibold">
+      <div className="flex justify-between items-center lg:m-10 mb-4 p-4 md:p-0">
+        <h1 className="text-foreground md:max-lg:m-6 text-xl md:text-4xl font-semibold">
           Property List
         </h1>
         <CreatePropertyButton />
