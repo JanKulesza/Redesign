9128b325b8721e21180409cd07fc578e64388fa8
Jan Kulesza <kulesz.jasiek@gmail.com>
2025-01-10 19:53:25 +0100

    Display current user in NavBar

--- client/src/components/Layout/NavBar.tsx
+++ client/src/components/Layout/NavBar.tsx
@@ -11,12 +11,16 @@ import { Button } from "../ui/button";
 import { useSidebar } from "../ui/sidebar";
 import SearchInput from "./SearchInput";
 import { ModeToggle } from "./ModeToggle";
-import avatar from "@/assets/avatar.jpg";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Link } from "react-router";
+import { useAuth } from "@/context/AuthProvider";
+import { useAuthUserId, useUser } from "@/hooks/useUsers";
+import { User } from "@/entities/User";
 
 const NavBar = () => {
   const { isMobile, setOpenMobile } = useSidebar();
+  const { token } = useAuth();
+  const user = token ? useUser(useAuthUserId(token)) : ({} as User);
 
   return (
     <nav className="md:px-10 max-h-20 flex-1 flex p-4 justify-between bg-background">
@@ -37,14 +41,19 @@ const NavBar = () => {
           <DropdownMenuTrigger>
             <div className="flex text-foreground gap-2 align-items-center">
               <Avatar className="max-h-[48px] h-[48px] max-w-[48px] w-[48px]">
-                <AvatarImage src={avatar} className="rounded-3xl" />
+                <AvatarImage
+                  src={`http://localhost:8080/api/v1/public/${user.avatar}`}
+                  className="rounded-3xl"
+                />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
-              <div className="text-left hidden lg:block">
-                <h5>Adam Smith</h5>
-                <span className="text-secondary-foreground">
-                  Company Manager
-                </span>
+              <div className="text-left hidden lg:flex lg:flex-col justify-center">
+                <h5>{user.firstName + " " + user.lastName}</h5>
+                {user.position && (
+                  <span className="text-secondary-foreground">
+                    {user.position}
+                  </span>
+                )}
               </div>
               <ChevronDown className="h-[48px]" />
             </div>
